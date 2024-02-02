83738a72e10b575a6c5fd5aca28ec48593bd7b51
Maria Petrova <maria-petrova1296@yandex.ru>
2024-02-02 11:00:28 +0300

    second commut
    3 commit
    
    4 commit
    
    5 commit
    join commit

--- precode.go
+++ precode.go
@@ -44,18 +44,24 @@ var tasks = map[string]Task{
 // Ниже напишите обработчики для каждого эндпоинта
 // Обработчик для получения всех задач
 func getTasks(w http.ResponseWriter, r *http.Request) {
-	//сериализуем данные из слайса tasks
+	// Сериализуем данные из слайса tasks
 	resp, err := json.Marshal(tasks)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Printf("getTasks json.Marshal error %v\n", err)
 		return
 	}
-	// в заголовок записываем тип контента, у нас это данные в формате JSON
+	// В заголовок записываем тип контента, у нас это данные в формате JSON
 	w.Header().Set("Content-Type", "application/json")
-	//устанавливаем статус ответа 200 ок
+	// Устанавливаем статус ответа 200 ок
 	w.WriteHeader(http.StatusOK)
-	//записывает данные в формате JSON в тело ответа
-	w.Write(resp)
+	// записывает данные в формате JSON в тело ответа
+	_, err = w.Write(resp)
+	if err != nil {
+		fmt.Printf("getTasks w.Write error %v", err)
+		return
+	}
+
 }
 
 // Обработчик для отправки задачи на сервер
@@ -63,38 +69,34 @@ func postTask(w http.ResponseWriter, r *http.Request) {
 	var task Task
 	var taskByte bytes.Buffer
 
-	//Читаем тело запроса
+	// Читаем тело запроса
 	_, err := taskByte.ReadFrom(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Printf("postTask taskByte.ReadFrom error %v\n", err)
 		return
 	}
 
-	//Десериализуем данные из JSON тела запроса и записываем их в переменную
+	// Десериализуем данные из JSON тела запроса и записываем их в переменную
 	err = json.Unmarshal(taskByte.Bytes(), &task)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Printf("postTask json.Unmarshal error %v\n", err)
 		return
 	}
 
-	//Проверяем, если в мапе task задада с ключом, который совпадает с id новой задачи
+	// Проверяем, если в мапе task задада с ключом, который совпадает с id новой задачи
 	_, ok := tasks[task.ID]
 	if !ok {
-		//Записываем новую задачу в мапу, id указанный в задаче будет соответсвовать ключу,
-		//под которым эта задача запишется в мапу
 		tasks[task.ID] = task
-		//Добавляем заголовок
-		w.Header().Set("Content-Type", "application/json")
-		//Возвращаем статус
 		w.WriteHeader(http.StatusCreated)
-	} else {
-		//Возвращаем статус конфликта, т.к. в мапе есть задача с таким id
-		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+	w.WriteHeader(http.StatusBadRequest)
 }
 
 // Обработчик для получения задачи по ID
-func getTaskByID(w http.ResponseWriter, r *http.Request) {
+func getTaskById(w http.ResponseWriter, r *http.Request) {
 	id := chi.URLParam(r, "id")
 
 	task, ok := tasks[id]
@@ -105,11 +107,16 @@ func getTaskByID(w http.ResponseWriter, r *http.Request) {
 	resp, err := json.Marshal(task)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Printf("postTaskById json.Marshal error %v\n", err)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
+	_, err = w.Write(resp)
+	if err != nil {
+		fmt.Printf("getTaskById w.Write error %v", err)
+		return
+	}
 }
 
 // Обработчик удаления задачи по ID
@@ -123,23 +130,17 @@ func deleteTaskById(w http.ResponseWriter, r *http.Request) {
 
 	delete(tasks, id)
 
-	resp, err := json.Marshal(tasks)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(resp)
 }
 
 func main() {
 	r := chi.NewRouter()
 
-	// здесь регистрируйте ваши обработчики
+	// Здесь регистрируйте ваши обработчики
 	r.Get("/tasks", getTasks)
 	r.Post("/tasks", postTask)
-	r.Get("/tasks/{id}", getTaskByID)
+	r.Get("/tasks/{id}", getTaskById)
 	r.Delete("/tasks/{id}", deleteTaskById)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
